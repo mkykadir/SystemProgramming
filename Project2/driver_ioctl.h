0679640d089f1276472809949fb6f18f89160831
Mahmut Lutfullah Özbilen <mahmutlozbilen@gmail.com>
2017-11-04 12:22:47 +0300

    driver_ioctl.h updated and magic values are added

--- Project2/driver_ioctl.h
+++ Project2/driver_ioctl.h
@@ -1,8 +1,14 @@
 #ifndef DRIVER_IOCTL_H
 #define DRIVER_IOCTL_H
 
-#define SET_READ_MODE 0
-#define SET_UNREAD_LIMIT 1
-#define CLEAR_MESSAGE_OF 2
+#include <linux/ioctl.h>
+
+#define IOC_MAGIC_READ 0
+#define IOC_MAGIC_LIMIT 1
+#define IOC_MAGIC_CLEAR_MESSAGE 2
+
+#define SET_READ_MODE _IOW(IOC_MAGIC_READ, 0, int)
+#define SET_UNREAD_LIMIT _IOW(IOC_MAGIC_LIMIT, 0, int)
+#define CLEAR_MESSAGE_OF _IOW(IOC_MAGIC_CLEAR_MESSAGE, 0, int)
 
 #endif
